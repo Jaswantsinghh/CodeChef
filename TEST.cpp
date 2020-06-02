10a452b6ea0ce69b1cba533efe5888b93d0fbbb3
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-06-03 01:13:56 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long t;
+    while(1)
+    {
+        cin >> t;
+        if(t==42)
+        {
+            break;
+        }
+        cout << t << "\n";
+    }
+	return 0;
+}
