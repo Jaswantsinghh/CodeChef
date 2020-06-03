e2923ed100c7901b13ced545ecf0f1abffeaec2b
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-06-04 01:18:53 +0530

    Create FSQRT.cpp

---
+++ FSQRT.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long t,num;
+    cin >> t;
+    while(t--)
+    {
+        cin >> num;
+        int sq=sqrt(num);
+        cout << sq <<"\n";
+    }
+	// your code goes here
+	return 0;
+}
