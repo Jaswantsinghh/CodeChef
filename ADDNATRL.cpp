11c67b5103567e9be39c9df92743990905f9953d
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-06-14 17:36:33 +0530

    Create ADDNATRL.cpp

---
+++ ADDNATRL.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    cout << (t*(t+1))/2<<"\n";
+	// your code goes here
+	return 0;
+}
+
